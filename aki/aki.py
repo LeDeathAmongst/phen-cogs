b54e0c64c99bca2d1d63df77f217eb9eeed044e7
LeDeathAmongst <180091824+LeDeathAmongst@users.noreply.github.com>
2024-09-28 19:43:31 -0400

    Update aki.py

--- aki/aki.py
+++ aki/aki.py
@@ -1,27 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2020-present phenom4n4n
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 import logging
 import aiohttp
 import akinator
@@ -30,13 +6,14 @@ from akinator.async_aki import Akinator
 from redbot.core import commands
 from redbot.core.bot import Red
 from redbot.core.config import Config
-from Star_Utils import Cog
+
 from .views import AkiView, channel_is_nsfw
 
 log = logging.getLogger("red.phenom4n4n.aki")
 
+VALID_LANGUAGES = {"en", "fr", "es", "de", "it", "nl", "pt", "tr", "ar", "ru", "jp"}
 
-class Aki(Cog):
+class Aki(commands.Cog):
     """
     Play Akinator in Discord!
     """
@@ -50,6 +27,12 @@ class Aki(Cog):
         )
         self.session = aiohttp.ClientSession()
 
+    __version__ = "1.2.0"
+
+    def format_help_for_context(self, ctx):
+        pre_processed = super().format_help_for_context(ctx)
+        n = "\n" if "\n\n" not in pre_processed else ""
+        return f"{pre_processed}{n}\nCog Version: {self.__version__}"
 
     async def red_delete_data_for_user(self, *, requester: str, user_id: int) -> None:
         return
@@ -64,6 +47,13 @@ class Aki(Cog):
         """
         Start a game of Akinator!
         """
+        if language not in VALID_LANGUAGES:
+            await ctx.send(
+                "Invalid language. Please choose from the following: "
+                + ", ".join(VALID_LANGUAGES)
+            )
+            return
+
         await ctx.typing()
         aki = Akinator()
         child_mode = not channel_is_nsfw(ctx.channel)
@@ -77,6 +67,9 @@ class Aki(Cog):
             await ctx.send(
                 "Invalid language. Refer here to view valid languages.\n<https://github.com/NinjaSnail1080/akinator.py#functions>"
             )
+        except AttributeError as e:
+            log.error("An AttributeError occurred: %s", e)
+            await ctx.send("An unexpected error occurred while starting the game. Please try again.")
         except Exception as e:
             log.error("An error occurred while starting the Akinator game: %s", e)
             await ctx.send("I encountered an error while connecting to the Akinator servers.")
